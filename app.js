a0bbc94ff4bda179886625d951e5466900e65ff1
jsrubin <justin.rubin@gmail.com>
2016-05-22 06:23:29 -0700

    added humor and trash talk speach bubble module

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ var url = require('url');
 
 var rankFILE = 'public/resources/ranking.json';
 var rankingJSON = path.join(__dirname, 'public/resources/ranking.json');
+var humorJSON = path.join(__dirname, 'public/resources/humor.json');
 var rankData = require('./public/resources/ranking.json');
+var humor = require('./public/resources/humor.json');
 
 
 app.set('port', 8080);
@@ -26,14 +28,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/leaderboard', function(req, res) {
 	fs.readFile(rankingJSON, function(err, data) {
 
-	var payload = { "ranking": JSON.parse(data).payload };
+	var ranking = { "ranking": JSON.parse(data).payload };
+ 	var payload = _.extend(ranking, humor);
 
   	res.json(payload);
   });
 });
 
 app.post('/leaderboard', function(req, res) {
-	var payload = { "ranking": req.body.payload };
+	var ranking = { "ranking": req.body.payload };
+ 	var payload = _.extend(ranking, humor);
 
 	fs.writeFile(rankFILE, JSON.stringify(req.body), function (err) {
 	  if (err) return console.log(err)
--- public/script/index.js
+++ public/script/index.js
@@ -12,6 +12,9 @@ var Board = React.createClass({
   		return {
   			url: '',
 	    	ranking: [],
+	    	newTopThree: [],
+	    	topThree: [],
+	    	humor: [],
 	    	trash: []
 	    };
 	},
@@ -33,9 +36,15 @@ var Board = React.createClass({
 	      dataType: 'json',
 	      cache: false,
 	      success: function(data) {
+	              var newTopThree = _.filter(data.payload, function (top) {
+	              	return top.rank == 1 || top.rank == 2 || top.rank == 3;
+	              });
+
 	              this.setState({
 			    	ranking: data.ranking,
-			    	trash: data.change
+			    	newTopThree: newTopThree,
+			    	humor: data.humor,
+			    	trash: data.trashTalk
 	              });
 
 	      }.bind(this),
@@ -43,12 +52,26 @@ var Board = React.createClass({
 	        console.error(this.props.url, status, err.toString());
 	      }.bind(this)
 	    });
-	},
+	  },
+
+	renderHumor: function () {
+		return (
+		    React.createElement(HumorModule, {
+		    	newTopThree: this.state.newTopThree,
+		    	topThree: this.state.topThree,
+	    		humor: this.state.humor,
+	    		trash: this.state.trash
+		    	})
+		);
+    },
 
 	renderLeaderboard: function () {
 		return (
 		    React.createElement(Leaderboard, {
-	    		ranking: this.state.ranking
+	    		ranking: this.state.ranking,
+	    		newTopThree: this.state.newTopThree,
+		    	topThree: this.state.topThree,
+	    		renderHumor: this.renderHumor()
 		    	})
 		);
     },
@@ -76,6 +99,40 @@ function cx(map) {
   return className.join(' ');
 }
 
+var HumorModule = React.createClass({
+	render: function () {
+		var humor = this.props.humor;
+		var trash = this.props.trash;
+		var newTop = this.props.newTopThree;
+		var top = this.props.topThree;
+
+		if (humor.length > 0) {
+			var leaderChange = _.difference(top, newTop).length > 0 ? true : false;
+			if (leaderChange) {
+				humor = trash;
+			}
+
+			var r = Math.floor(Math.random() * (humor.length));
+
+			var humorText = [];
+			humorText.push(humor[r].text);
+			var cls = humor[r].style;
+	    	var rowClass = cx({
+							triangle: true,
+							right: cls == 'right' ? true : false,
+							left: cls == 'left' ? true : false,
+							top: cls == 'top' ? true : false,
+						});
+			return (
+		  		<p className={rowClass}><i className="humor">"{humorText}"</i></p>
+		  		);
+		}
+		return (
+			<p></p>
+			);
+	}
+})
+
 var Leaderboard = React.createClass({
 
 	score: function (wins, losses) {
@@ -98,7 +155,7 @@ var Leaderboard = React.createClass({
 		}).reverse();
 	},
 
-	leaderRowsTop: function (ranking, displayCount) {
+	leaderRowsTop: function (ranking, displayCount, renderHumor) {
 		return ranking.map(function(leader, index) {
 				var rank = index + 1;
 		    	var isPrimary = (rank % 2 === 1) ? true : false;
@@ -108,12 +165,19 @@ var Leaderboard = React.createClass({
 							});
 		    	var metal = [];
 
+				var rand = Math.floor(Math.random() * (displayCount));
+
 		    	if (rank == 1) {
 		    		metal.push(<img src="resources/gold.png"/>);
 		    	} if (rank == 2) {
 		    		metal.push(<img src="resources/silver.png"/>);
 		    	} if (rank == 3) {
 		    		metal.push(<img src="resources/bronze.png"/>);
+		    	}
+
+		    	var humor = [];
+		    	if (rank == rand) {
+		    		humor.push(renderHumor);
 		    	}
 
 		    	if (rank <= displayCount) {
@@ -140,13 +204,45 @@ var Leaderboard = React.createClass({
 			});
 	},
 
+	leaderRowsUpcoming: function (ranking) {
+		return ranking.map(function(leader) {
+		    	var isPrimary = (leader.rank % 2 === 1) ? true : false;
+		    	var rank = leader.rank;
+		    	var rowClass = cx({
+								rank: true,
+								primary: {isPrimary} 
+							});
+		    	if (rank == 4 || rank == 5 || rank == 6) {
+			        return (
+							<tr className="rowChallenger">
+								<th scope="row"
+								className={cx({ 
+									row: true,
+									primary: {isPrimary}
+								})}>
+								</th><td 
+								className={rowClass}>
+								{leader.name}</td>
+								<td></td>
+							</tr>
+				     );
+			    }
+			});
+	},
+
 	render: function() {
+		var newTopThree = this.props.newTopThree;
+		var topThree = this.props.topThree;
 		var leaderRows = [];
+		var upcomingLeaderRows = [];
+		var renderHumor = this.props.renderHumor;
+
 		var displayCount = 6;
 
 		if (this.props.ranking && this.props.ranking.length > 0) {
 			var ranking = this.ranking(this.props.ranking);
-			leaderRows = this.leaderRowsTop(ranking, displayCount);
+			leaderRows = this.leaderRowsTop(ranking, displayCount, renderHumor);
+			// upcomingLeaderRows = this.leaderRowsUpcoming(this.props.ranking);
 	    }
 	    return (
 			<div className="top6">
